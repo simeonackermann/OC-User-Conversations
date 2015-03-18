5547437faa27c5c00c99e52acdd4f6d549a512cb
Simeon Ackermann <amseon@web.de>
2015-03-19 00:43:02 +0200

    Add js and json translations (OC > 8 compatibility)

---
+++ l10n/fr.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "conversations",
+    {
+    "Conversation" : "Conversation",
+    "Groups" : "Groupes",
+    "User" : "Usager",
+    "Message"   : "Message",
+    "Submit"    : "Publier",
+    "Add file" : "Ajouter un fichier",
+    "Select file" : "Sélectionner le fichier",
+    "The file will be shared with the %s group." : "Le fichier est partagé avec le groupe %s.",
+    "The file will be shared with the user %s." : "Le fichier est partagé avec %s.",
+    "Loading older comments" : "Chargement des anciens commentaires...",
+    "No more comments to load" : "Pas plus de commentaires à charger."
+},
+"nplurals=2; plural=(n > 1);");
---
+++ l10n/it.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "conversations",
+    {
+    "Conversation" : "Messaggi",
+    "Groups" : "Gruppi",
+    "User" : "Utenti",
+    "Message"   : "Messaggio",
+    "Submit"    : "Invia",
+    "Add file" : "Allega un file",
+    "Select file" : "Scegli file da allegare",
+    "The file will be shared with the %s group." : "Il documento sarà condiviso con il gruppo %s.",
+    "The file will be shared with the user %s." : "Il documento sarà condiviso con %s.",
+    "Loading older comments" : "Sto caricando altri messaggi...",
+    "No more comments to load" : "Nessun altro messaggio. Yuppi!"
+},
+"nplurals=2; plural=(n != 1);");
