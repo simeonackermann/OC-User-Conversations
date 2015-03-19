8c685d3c7d7c02e17945563ba29283b57925c340
Simeon Ackermann <amseon@web.de>
2015-03-19 23:49:20 +0200

    Add globael header menu notification icon for OC > 8

--- js/updateCheck.js
+++ js/updateCheck.js
@@ -1,28 +1,33 @@
 OC.Conversations_App = {
 
 	updateCheck : function() {
-
 		$.post(OC.filePath('conversations', 'ajax', 'polling.php'), function(jsondata) {
-            if(jsondata.status == 'success' && jsondata.data.length != 0 ) {
-            	
-            	var iconSrc = OC.appswebroots.conversations + "/img/conversations_red.png";
-            	$('#navigation li[data-id="conversations_index"] img').attr ("src", iconSrc );
-            	
-            	//$('#navigation li[data-id="conversations"] a').attr ("title", 'There are new messages' );
+			if(jsondata.status == 'success' && jsondata.data.length != 0 ) {
+				// update conversations app-menu icon
+				var iconSrc = OC.appswebroots.conversations + "/img/conversations_red.png";
+				$('#navigation li[data-id="conversations_index"] img').attr ("src", iconSrc );
 
-            }
-        }, 'json');
+				// add notif-icon in header bar for OC > 8
+				if ( parseInt(OC.config.version) >= 8 && $(".conversations-notification").length < 1 ) {
+					var conversationsUrl = OC.generateUrl('apps/conversations/');
+					$("#header .menutoggle").css("left", "105px");
+					$("#header .menutoggle").after('<div class="menutoggle conversations-notification" style="position:absolute;left:70px;padding-top:24px">' + 
+						'<a href="'+conversationsUrl+'" title="'+t('conversations', 'Conversation')+": "+t('conversations', 'New comments') +'" class="header-appname">' +
+							'<img src="'+iconSrc+'" style="width:80%" />'+
+						'</a></div>');
+				}
+			}
+		}, 'json');
 	}
 
 }
 
 $(document).ready(function(){
 
+	// do updateCheck if app is not active
 	if ( $('#navigation li[data-id="conversations_index"] a').attr('class') != "active" ) { // TODO: certainly not the best way...!
-
 		OC.Conversations_App.updateCheck();
 		setInterval( function(){ OC.Conversations_App.updateCheck(); }, 15000);
-
 	}
 
 });
