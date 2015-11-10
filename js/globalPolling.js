0ff5e8d3f3bb82cce33bde3e7cf8a3ae69863bd4
Simeon Ackermann <s.ackermann@mail.de>
2015-11-10 20:56:03 +0100

    Fix right alignment of global notif icon

--- js/globalPolling.js
+++ js/globalPolling.js
@@ -18,7 +18,7 @@ OC.Conversations_App = {
 					// add notif-icon in header bar for OC > 8
 					if ( parseInt(OC.config.version) >= 8 && $(".conversations-notification").length < 1 ) {
 						var conversationsUrl = OC.generateUrl('apps/conversations/');
-						$("#header #settings").after('<div class="conversations-notification" style="float:right;padding-top:22px;margin-right:-15px;">' + 
+						$("#header #settings").after('<div class="conversations-notification" style="float:right;padding-top:22px;margin-right:-10px;">' + 
 							'<a href="'+conversationsUrl+'" title="'+t('conversations', 'Conversation')+": "+t('conversations', 'New comments') +'" class="header-appname">' +
 								'<img src="'+iconSrc+'" style="width:80%" />'+
 							'</a></div>');
