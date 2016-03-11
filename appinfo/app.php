e0272335f2c5ee70294a312d9bc43bf88c1aebde
Simeon Ackermann <s.ackermann@mail.de>
2016-03-11 20:22:21 +0100

    Fix for OC 9.0.0, change getL10N

--- appinfo/app.php
+++ appinfo/app.php
@@ -40,7 +40,6 @@ OC_HOOK::connect('OC_User', 'post_removeFromGroup', 'OC_Conversations', 'changeU
 OC_HOOK::connect('OC_User', 'post_login', 'OC_Conversations', 'hook_login');
 OC_HOOK::connect('OC_User', 'logout', 'OC_Conversations', 'hook_logout');
 
-//$l = OC_L10N::get('conversations');
 $l = OCP\Util::getL10N('conversations');
 OCP\App::addNavigationEntry( array( 
 	'id' => 'conversations_index',
